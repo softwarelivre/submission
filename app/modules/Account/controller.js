3ea8586ab30f3ecc4725fdadad9a1cad383e31b6
softwarelivre <inscricoes@softwarelivre.org>
2016-02-29 11:29:49 -0300

    Refactor account module

--- app/modules/Account/controller.js
+++ app/modules/Account/controller.js
@@ -13,7 +13,8 @@
         .state('account', {
           url: '^/account/',
           views: {
-            "header": {templateUrl: 'modules/common/nav.html' }
+            "header": {templateUrl: 'modules/common/nav.html' },
+            "main":   { template:  "<div ui-view='form'></div>", controller: 'AccountController' }
           }
         });
     })
@@ -33,7 +34,7 @@
           parent: 'account',
           url: '^/account/signup',
           views: {
-            "main@":   { controller: 'SignUpController', templateUrl: 'modules/Account/baseform.html' },
+            "form":   { controller: 'SignUpController', templateUrl: 'modules/Account/baseform.html' },
           },
         });
     })
@@ -43,7 +44,7 @@
           parent: 'account',
           url: '^/account/update',
           views: {
-           "main@":   { controller: 'UpdateAccountController', templateUrl: 'modules/Account/baseform.html' },
+           "form":   { controller: 'UpdateAccountController', templateUrl: 'modules/Account/baseform.html' },
           },
         });
     });
@@ -72,8 +73,23 @@
                  .catch(FormErrors.set);
       };
     })
+    .controller("AccountController", function($scope, Account, AddressResolver) {
+      $scope.disabilityTypes =  Account.getDisabilityTypes();
+      $scope.occupationTypes = Account.getOccupationTypes();
+      $scope.educationTypes = Account.getEducationTypes();
+
+      $scope.getLocation = function(address) {
+        return AddressResolver.fetchLocation(address).then(function(results){
+            return results.map(function(item){
+                return item;
+            });
+        });
+      };
+
+
+    })
     .controller("SignUpController", function($scope,
-                                             Account, Auth, Validator, FormErrors, UserLocation,
+                                             Account, Auth, FormErrors, UserLocation,
                                              focusOn, $http, AddressResolver) {
 
       $scope.type = 'person';
@@ -85,14 +101,6 @@
 
       $scope.selectedAddress = '';
 
-      $scope.getLocation = function(address) {
-        return AddressResolver.fetchLocation(address).then(function(results){
-            return results.map(function(item){
-                return item;
-            });
-        });
-      };
-
       $scope.onSelectLocation = function($item){
         var address = AddressResolver.convertToAddress($item);
         $scope.signup.country = address.country;
@@ -102,10 +110,6 @@
         $scope.signup.address_neighborhood = address.neighborhood;
         $scope.signup.address_street = address.street;
       };
-
-      $scope.disabilityTypes =  Account.getDisabilityTypes();
-      $scope.occupationTypes = Account.getOccupationTypes();
-      $scope.educationTypes = Account.getEducationTypes();
 
       function finishedSignUp() {
         Auth.login($scope.signup.email, $scope.signup.password);
@@ -120,36 +124,28 @@
         if($scope.signup.membership === 'true') { $scope.signup.membership = true; }
         if($scope.signup.membership === 'false') { $scope.signup.membership = false; }
 
-         var schema =  'accounts/signup';
-         if($scope.type == 'corporate')  {
-             schema = 'accounts/corporate';
-         }
-
-        Validator.validate($scope.signup, schema)
-                 .then(Account.post)
-                 .then(Account.localForget)
-                 .then(finishedSignUp)
-                 .catch(FormErrors.set);
+        Account.post($scope.signup)
+               .then(finishedSignUp)
+               .catch(FormErrors.setError);
       };
     })
     .controller("UpdateAccountController", function($scope, $state,
-                                             Account, Auth, AuthModal, Validator, FormErrors, UserLocation,
+                                             Account, Auth, AuthModal, FormErrors, UserLocation,
                                              focusOn, AddressResolver, ngToast) {
 
       $scope.enforceAuth();
+      $scope.credentials = Auth.glue($scope, 'credentials');
 
       $scope.selectedAddress = '';
 
-      $scope.getLocation = function(address) {
-        return AddressResolver.fetchLocation(address).then(function(results){
-            return results.map(function(item){
-                return item;
-            });
-        });
-      };
+      $scope.signup = {};
+
+      $scope.lockEmail = true;
+      $scope.lockType = true;
 
       $scope.onSelectLocation = function($item){
         var address = AddressResolver.convertToAddress($item);
+        console.log('ododo');
         $scope.signup.country = address.country;
         $scope.signup.address_state = address.state;
         $scope.signup.city = address.city;
@@ -157,13 +153,6 @@
         $scope.signup.address_neighborhood = address.neighborhood;
         $scope.signup.address_street = address.street;
       };
-
-      $scope.credentials = Auth.glue($scope, 'credentials');
-
-      $scope.signup = {};
-
-      $scope.lockEmail = true;
-      $scope.lockType = true;
 
       Account.get().then(function(data){
         $scope.signup = data;
@@ -173,21 +162,20 @@
         {
            delete $scope.signup.document
         }
+        /* FIX PERMISSIONS */
+        $scope.type = 'person'
         if( $scope.signup.role == 'corporate')
         {
             $scope.type = 'corporate';
-            $scope.signup.cnpj = $scope.signup.document
-        } else if ( $scope.signup.role == 'person') {
-            $scope.signup.cpf = $scope.signup.document
+            $scope.signup.cnpj = $scope.signup.document;
+        } else if ( $scope.signup.role == 'user') {
+            $scope.signup.cpf = $scope.signup.document;
         } else if( $scope.signup.role == 'foreign') {
-            $scope.signup.passport = $scope.signup.document
+            $scope.type = 'foreign'
+            $scope.signup.passport = $scope.signup.document;
         }
         delete $scope.signup.document;
       });
-
-      $scope.disabilityTypes =  Account.getDisabilityTypes();
-      $scope.occupationTypes = Account.getOccupationTypes();
-      $scope.educationTypes = Account.getEducationTypes();
 
       function finishedUpdate() {
         ngToast.create({ content: 'Sua conta foi atualizada com sucesso.' }); //FIX ME
@@ -196,19 +184,12 @@
       }
 
       $scope.submit = function() {
+          if($scope.signup.membership === 'true') { $scope.signup.membership = true; }
+          if($scope.signup.membership === 'false') { $scope.signup.membership = false; }
 
-            if($scope.signup.membership === 'true') { $scope.signup.membership = true; }
-            if($scope.signup.membership === 'false') { $scope.signup.membership = false; }
-
-            var schema =  'accounts/edit_account';
-            if($scope.type == 'corporate')  {
-                schema = 'accounts/edit_corporate';
-            }
-
-            Validator.validate($scope.signup, schema)
-                     .then(Account.saveIt)
-                     .then(finishedUpdate)
-                     .catch(FormErrors.set);
+          Account.saveIt($scope.signup)
+                 .then(finishedUpdate)
+                 .catch(FormErrors.setError);
       };
     });
 })();
